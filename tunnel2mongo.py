a8cc558c16a22c098a0c70b7813eb724e62a057c
Sanal Kumar <sanat@cisco.com>
2019-04-03 21:52:47 -0700

    Create Mongo tunnel for running local CRUD operations

---
+++ tunnel2mongo.py
@@ -0,0 +1,17 @@
+# Create tunnel to mongo in remote machine and do CRUD operations on Mongodb
+
+from sshtunnel import SSHTunnelForwarder
+import pymongo
+
+def create_tunnel_and_mongo_operation(args):
+    with SSHTunnelForwarder(
+            (args.ip, args.port),   # 192.168.1.1 , 8080
+            ssh_username="<vm_username>",
+            ssh_password="<vm_password>",
+            remote_bind_address=('127.0.0.1', 27017)
+    ) as tunnel:
+        client = pymongo.MongoClient('127.0.0.1', tunnel.local_bind_port)
+        db = client["<db_name>"]
+        db.authenticate('<dbusername>', "<dbpassword>")
+        db["notes"].delete_one({'id': "123"})
+    print("DB Operation complete")
